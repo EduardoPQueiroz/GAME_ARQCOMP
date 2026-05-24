e287a3865e9e43ea66a22b498b9f192c0db63402
EduardoPQueiroz <eduardo.queiroz@sptech.school>
2026-05-24 13:34:16 -0300

    organizing files

---
+++ public/dados.js
@@ -0,0 +1,148 @@
+let dialogos = ['Olá tudo bem?', 'Me chamo Du, vou ser seu guia na sua aventura em Tópicos de Arquitetura Computacional', 'Sua missão será responder a um total de 16 perguntas sobre tópicos de arquitetura computacional, mas cuidado, toda vez que você errar, vamos ter que começar tudo de novo! Está preparado?']
+
+let perguntas = [
+        {
+            'questao': '1. Pergunta interessante',
+            'alternativas': [
+                'Opção A',
+                'Opção B',
+                'Opção C',
+                'Opção D'
+            ]
+        },
+        {
+            'questao': '2. Pergunta interessante',
+            'alternativas': [
+                'Opção A',
+                'Opção B',
+                'Opção C',
+                'Opção D'
+            ]
+        },
+        {
+            'questao': '3. Pergunta interessante',
+            'alternativas': [
+                'Opção A',
+                'Opção B',
+                'Opção C',
+                'Opção D'
+            ]
+        },
+        {
+            'questao': '4. Pergunta interessante',
+            'alternativas': [
+                'Opção A',
+                'Opção B',
+                'Opção C',
+                'Opção D'
+            ]
+        },
+        {
+            'questao': '5. Pergunta interessante',
+            'alternativas': [
+                'Opção A',
+                'Opção B',
+                'Opção C',
+                'Opção D'
+            ]
+        },
+        {
+            'questao': '6. Pergunta interessante',
+            'alternativas': [
+                'Opção A',
+                'Opção B',
+                'Opção C',
+                'Opção D'
+            ]
+        },
+        {
+            'questao': '7. Pergunta interessante',
+            'alternativas': [
+                'Opção A',
+                'Opção B',
+                'Opção C',
+                'Opção D'
+            ]
+        },
+        {
+            'questao': '8. Pergunta interessante',
+            'alternativas': [
+                'Opção A',
+                'Opção B',
+                'Opção C',
+                'Opção D'
+            ]
+        },
+        {
+            'questao': '9. Pergunta interessante',
+            'alternativas': [
+                'Opção A',
+                'Opção B',
+                'Opção C',
+                'Opção D'
+            ]
+        },
+        {
+            'questao': '10. Pergunta interessante',
+            'alternativas': [
+                'Opção A',
+                'Opção B',
+                'Opção C',
+                'Opção D'
+            ]
+        },
+        {
+            'questao': '11. Pergunta interessante',
+            'alternativas': [
+                'Opção A',
+                'Opção B',
+                'Opção C',
+                'Opção D'
+            ]
+        },
+        {
+            'questao': '12. Pergunta interessante',
+            'alternativas': [
+                'Opção A',
+                'Opção B',
+                'Opção C',
+                'Opção D'
+            ]
+        },
+        {
+            'questao': '13. Pergunta interessante',
+            'alternativas': [
+                'Opção A',
+                'Opção B',
+                'Opção C',
+                'Opção D'
+            ]
+        },
+        {
+            'questao': '14. Pergunta interessante',
+            'alternativas': [
+                'Opção A',
+                'Opção B',
+                'Opção C',
+                'Opção D'
+            ]
+        },
+        {
+            'questao': '15. Pergunta interessante',
+            'alternativas': [
+                'Opção A',
+                'Opção B',
+                'Opção C',
+                'Opção D'
+            ]
+        },
+        {
+            'questao': '16. Pergunta interessante',
+            'alternativas': [
+                'Opção A',
+                'Opção B',
+                'Opção C',
+                'Opção D'
+            ]
+        }
+    ]
